66605b9be221b69795d82798f5c95e9a066bfbba
ohohoi <ohohoi@sookmyung.ac.kr>
2020-11-06 19:28:54 +0900

    Initial commit

---
+++ src/main/java/com/dsc/rankmakers/RankmakersApplication.java
@@ -0,0 +1,13 @@
+package com.dsc.rankmakers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RankmakersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RankmakersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dsc/rankmakers/RankmakersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsc.rankmakers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RankmakersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
