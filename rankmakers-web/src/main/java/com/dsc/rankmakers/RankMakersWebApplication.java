af1a7ebc1579c32069fee03c355924da50c8609a
Sindongsun <shindongsun0@naver.com>
2020-11-06 20:44:36 +0900

    construct multi-module project

--- rankmakers-web/src/main/java/com/dsc/rankmakers/RankMakersWebApplication.java
+++ rankmakers-web/src/main/java/com/dsc/rankmakers/RankMakersWebApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RankmakersApplication {
+public class RankMakersWebApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RankmakersApplication.class, args);
+        SpringApplication.run(RankMakersWebApplication.class, args);
     }
 
 }
