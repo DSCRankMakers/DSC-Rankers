7ffc1cbb2399e7fb137c0089c5c71dc42fe513fd
ohohoi <ohohoi@sookmyung.ac.kr>
2020-11-06 22:47:54 +0900

    Create OrganizationController.java

---
+++ rankmakers-web/src/main/java/com/dsc/rankmakers/domain/organization/api/OrganizationController.java
@@ -0,0 +1,8 @@
+package com.dsc.rankmakers.domain.organization.api;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrganizationController {
+    
+}
