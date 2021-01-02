77671404ebcafded8f08463952b191f2de633b5c
Sindongsun <shindongsun0@naver.com>
2021-01-03 00:27:10 +0900

    feat: oauth2 AppProperties를 클래스에 바인딩
    ConfigurationProperties 어노테이션을 사용해 POJO(Plain Old Java Object) 클래스에 바인딩해 사용한다.

--- rankmakers-web/src/main/java/com/dsc/rankmakers/RankMakersWebApplication.java
+++ rankmakers-web/src/main/java/com/dsc/rankmakers/RankMakersWebApplication.java
@@ -1,9 +1,12 @@
 package com.dsc.rankmakers;
 
+import com.dsc.rankmakers.domain.security.config.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AppProperties.class)
 public class RankMakersWebApplication {
 
     public static void main(String[] args) {
